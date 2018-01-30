d7da18696e48d3a80c8d291ea14bbde1bf5edfac
FABRICIO RESENDE GONCALVES <p061216@d02.prodemge.gov.br>
2018-01-30 12:36:42 -0200

    'initial'

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+      sass: {
+        dist: {
+          files: {
+            'public/stylesheets/style.css': 'sass/style.scss'
+          }
+        },
+        options: {
+          includePaths: [
+            './bower_components/css-calc-mixin'
+          ]
+        }
+      },
+      watch: {
+        source: {
+          files: ['sass/**/*.scss', 'views/**/*.jade'],
+          tasks: ['sass'],
+          options: {
+            livereload: true, // needed to run LiveReload
+          }
+        }
+      }
+    });
+  
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.registerTask('default', ['sass']);
+  };
